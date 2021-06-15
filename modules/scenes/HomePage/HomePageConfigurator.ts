30503856bd9ab0b6a1e5ecfb2626d60aad7684fd
aromal <aromalsasidharan@gmail.com>
2021-06-15 15:14:36 +0800

    [update] clean up

--- modules/scenes/HomePage/HomePageConfigurator.ts
+++
@@ -1,9 +0,0 @@
-import  {HomePagePresenter, AbstractHomePageView} from "./presenter/HomePagePresenter";
-
-
-class HomePageConfigurator {
-    // static configureHomePagePresenter(view: AbstractHomePageView): HomePagePresenter {
-    //     const presenter = new HomePagePresenter()
-    //     return  presenter
-    // }
-}
